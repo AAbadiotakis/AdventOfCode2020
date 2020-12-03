f9709002fdb6bfbf6028d237f390711ffeb372a6
Alex Abadiotakis <alexabadiotakis@gmail.com>
2020-12-03 00:32:07 -0500

    Advent Of Code Day3 Solution

---
+++ src/main/java/style/hidden/day3/Day3Runner.java
@@ -0,0 +1,51 @@
+package style.hidden.day3;
+
+import style.hidden.FileReaderUtil;
+
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+public class Day3Runner {
+
+    static int treeCounter(int moveX, int moveY, Map<Integer, String> map, int maxRows) {
+        int treeCounter = 0;
+        int x = 0;
+        int y = 0;
+        while(true) {
+            x += moveX;
+            y += moveY;
+            if (y >= maxRows) {
+                return treeCounter;
+            }
+            String currentRow = map.get(y);
+            while(x >= currentRow.length()) {
+                currentRow += currentRow;
+            }
+            if(Character.toString(currentRow.charAt(x)).equals("#")) {
+                treeCounter++;
+            }
+        }
+    }
+
+
+    public static void main(String... args) throws Exception {
+        List<String> lines = FileReaderUtil.parseFile("day3input.txt");
+        Map<Integer, String> map = new HashMap<>();
+        int i = 0;
+        for(String line : lines) {
+            map.put(i, line);
+            i++;
+        }
+
+        int moveX3moveY1 = treeCounter(3, 1, map, i);
+        System.out.println("Day 3 Part 1 Answer = " + moveX3moveY1);
+
+        int moveX1moveY1 = treeCounter(1, 1, map, i);
+        int moveX5moveY1 = treeCounter(5, 1, map, i);
+        int moveX7moveY1 = treeCounter(7, 1, map, i);
+        int moveX1moveY2 = treeCounter(1, 2, map, i);
+
+        System.out.println("Day 3 Part 1 Answer = " + (moveX3moveY1 * moveX1moveY1 * moveX5moveY1 * moveX7moveY1 * moveX1moveY2));
+    }
+}
